6e2cb52a5347ebba871700a3b25431b58b111b00
Madhubethina1 <116302135+Madhubethina1@users.noreply.github.com>
2022-12-11 04:42:14 +0530

    Added Shop

--- resolver/app/src/main/java/uk/ac/tees/aad/W9507024/shop.java
+++ resolver/app/src/main/java/uk/ac/tees/aad/W9507024/shop.java
@@ -2,7 +2,10 @@ package uk.ac.tees.aad.W9507024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class shop extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        findViewById(R.id.buy1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+                httpIntent.setData(Uri.parse("https://www.stoptopps.com/"));
+                startActivity(httpIntent);
+            }
+        });
+        findViewById(R.id.buy2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+                httpIntent.setData(Uri.parse("https://www.drinkstuff.com/products/drink-safety.asp"));
+                startActivity(httpIntent);
+            }
+        });
+        findViewById(R.id.buy3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+                httpIntent.setData(Uri.parse("https://www.amazon.co.uk/drink-spiking-prevention/s?k=drink+spiking+prevention"));
+                startActivity(httpIntent);
+            }
+        });
     }
 }
